da2c6b2e46a6ce01d9719faf13ea3a7cb61cbdc2
DejingMa <dejingma@yufuid.com>
2021-01-22 14:04:44 +0800

    feat(core): export TestButton
    affects: @yufud/core

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -7,3 +7,4 @@ export { default as Alert } from './components/Alert';
 export { default as Button } from './components/Button';
 export { default as PromiseButton } from './components/PromiseButton';
 export { default as ImageTest } from './components/ImageTest';
+export { default as TestButton } from './components/TestButton';
